66a5e6eb94d7d5d3280d564482fb47274e6e15ea
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-01-01 16:59:45 +0800

    Up

---
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceRendererConfigFragment.java
@@ -0,0 +1,32 @@
+package net.kdt.pojavlaunch.prefs.screens;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import net.kdt.pojavlaunch.R;
+import net.kdt.pojavlaunch.prefs.LauncherPreferences;
+
+//Experimental Settings for Mesa renderer
+public class LauncherPreferenceRendererConfigFragment extends LauncherPreferenceFragment {
+
+    @Override
+    public void onCreatePreferences(Bundle b, String str) {
+        addPreferencesFromResource(R.xml.pref_renderexp);
+        computeVisibility();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences p, String s) {
+        super.onSharedPreferenceChanged(p, s);
+        computeVisibility();
+    }
+
+    private void computeVisibility(){
+        requirePreference("ZinkF").setVisible(LauncherPreferences.PREF_EXP_SETUP);
+        requirePreference("ZinkS").setVisible(LauncherPreferences.PREF_EXP_SETUP);
+        requirePreference("VulkanLwarlip").setVisible(LauncherPreferences.PREF_EXP_SETUP);
+        requirePreference("Rvirpipe").setVisible(LauncherPreferences.PREF_EXP_SETUP);
+        requirePreference("Rpanfrost").setVisible(LauncherPreferences.PREF_EXP_SETUP);
+        requirePreference("Rfreedreno").setVisible(LauncherPreferences.PREF_EXP_SETUP);
+    }
+}
