79be1f9bef316995fc21adcc3fe4d2595fca9f2b
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-01-07 10:41:21 +0800

    Update

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceRendererConfigFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceRendererConfigFragment.java
@@ -13,6 +13,31 @@ public class LauncherPreferenceRendererConfigFragment extends LauncherPreference
     public void onCreatePreferences(Bundle b, String str) {
         addPreferencesFromResource(R.xml.pref_renderexp);
         computeVisibility();
+
+        // Get RadioGroup Preference
+        final PreferenceCategory radioGroupPref = (PreferenceCategory) findPreference("radioGroupPref");
+        // Adding a Listener for an Option in a RadioGroup
+        for (int i =0; i < radioGroupPref.getPreferenceCount(); i++) {
+            radioGroupPref.getPreference(i).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    // Set Selected Status
+                    for (int i =0; i < radioGroupPref.getPreferenceCount(); i++) {
+                        radioGroupPref.getPreference(i).setChecked(false);
+                    }
+                    preference.setChecked(true);
+                    // Perform the appropriate action
+                    if (preference.getKey().equals("option1")) {
+                        // Perform the operation of option one
+                    } else if (preference.getKey().equals("option2")) {
+                        // Perform the operation of option 2
+                    } else if (preference.getKey().equals("option3")) {
+                        // Perform the operation of option three
+                    }
+                    return true;
+                }
+            });
+        }
     }
 
     @Override
