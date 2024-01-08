2ff97666afed1ebf53a8ced573a6059b660c53f2
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-01-08 19:49:35 +0800

    Update

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceRendererConfigFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceRendererConfigFragment.java
@@ -32,17 +32,17 @@ public class LauncherPreferenceRendererConfigFragment extends LauncherPreference
                     ((SwitchPreference) preference).setChecked(true);
                     // Perform the appropriate action
                     if (preference.getKey().equals("ZinkF")) {
-                        Toast.makeText(context, "Default", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Default", Toast.LENGTH_SHORT).show();
                     } else if (preference.getKey().equals("ZinkS")) {
-                        Toast.makeText(context, "Test", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Test", Toast.LENGTH_SHORT).show();
                     } else if (preference.getKey().equals("VulkanLwarlip")) {
-                        Toast.makeText(context, "Test", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Test", Toast.LENGTH_SHORT).show();
                     } else if (preference.getKey().equals("Rvirpipe")) {
-                        Toast.makeText(context, "Test", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Test", Toast.LENGTH_SHORT).show();
                     } else if (preference.getKey().equals("Rpanfrost")) {
-                        Toast.makeText(context, "Test", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Test", Toast.LENGTH_SHORT).show();
                     } else if (preference.getKey().equals("Rfreedreno")) {
-                        Toast.makeText(context, "Test", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Test", Toast.LENGTH_SHORT).show();
                     }
                     return true;
                 }
